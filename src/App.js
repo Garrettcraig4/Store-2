1cf0114bca99f82c07a767ec67ee268d09e0999d
Garrett <Garrettcrg@gmail.com>
2018-06-04 18:31:11 -0500

    rednered-header-component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Home />
       </div>
     );

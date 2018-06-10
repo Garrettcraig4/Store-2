770c57bc86b3639f00fd419999f8b400970b2cf4
Garrett <Garrettcrg@gmail.com>
2018-06-10 00:22:27 -0500

    working-link

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <h1>Welcome To The Store</h1>
-        <a>Shop</a>
+        <Link to="/Store">
+          <a>Store</a>
+        </Link>
       </div>
     );
   }

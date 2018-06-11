3e391ab989d907bcfb1baecd8455582113e41ad6
Garrett <Garrettcrg@gmail.com>
2018-06-11 00:32:52 -0500

    added-home-link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <p>Home</p>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
         <p>Store</p>
       </div>
     );

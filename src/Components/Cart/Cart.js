a3c64a3d6dba641ee588c41b8930696dca9044b3
Garrett <Garrettcrg@gmail.com>
2018-06-14 23:06:12 -0500

    added-cart-component

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Cart extends Component {
+  render() {
+    return (
+      <div className="Cart">
+        <p>cart</p>
+      </div>
+    );
+  }
+}
+
+export default Cart;
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Store from "./Components/Store/Store";
+import Cart from "./Components/Cart/Cart";
+
 export default (
   <Switch>
     <Route component={Home} exact path="/" />
     <Route component={Store} path="/Store" />
+    <Route component={Cart} path="/Cart" />
   </Switch>
 );
